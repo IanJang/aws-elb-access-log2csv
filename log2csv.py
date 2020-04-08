919accc9d4b3dae5cc01cd90d2a94a3d29d8aec4
ianjang <ianjang0718@gmail.com>
2020-04-08 19:00:45 +0900

    add field_names configuration

--- log2csv.py
+++ log2csv.py
@@ -1,6 +1,8 @@
 import getopt
 import sys
 import os
+
+from config import FIELD_NAMES
 from utils import aws_elb_access_log2csv
 
 
@@ -14,6 +16,8 @@ def main(argv):
     except getopt.GetoptError:
         print(help_text)
         sys.exit(2)
+
+    config = None
     for opt, arg in opts:
         if opt == '-h':
             print(help_text)
@@ -27,8 +31,9 @@ def main(argv):
         print("error message :", f"'{inputfile}' file does not exist")
         sys.exit(2)
 
+    field_names = FIELD_NAMES
     try:
-        aws_elb_access_log2csv(inputfile, outputfile)
+        aws_elb_access_log2csv(inputfile, outputfile, field_names=field_names)
     except Exception as e:
         print("command fail")
         print("error message :", e)
